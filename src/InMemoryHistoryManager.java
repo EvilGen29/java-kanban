64a33748e35582f4796f24062adefb29a81385c8
EvilG <khaninja29@yandex.ru>
2024-12-02 19:05:27 +0300

    тз6 в процессе(добавить тесты,проверить)

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,21 +1,70 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private static final int historyTasksSize = 10;
-    List<Task> historyTasks = new ArrayList<>();
+    Node first;
+    Node last;
+
+    Map<Integer,Node> nodes = new HashMap<>();
+
+    public void linkLast(Task task) {
+        Node newNode = new Node(last, task, null);
+        if (last == null) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        last = newNode;
+    }
+
+    public void removeNode(int id) {
+        Node node = nodes.remove(id);
+        if (node == null) {
+            return;
+        }
+        if (node.prev == null) {
+            first = first.next;
+            if (first == null) {
+                last = null;
+            } else {
+                first.prev = null;
+            }
+        } else if (node.next == null){
+            last = last.prev;
+            last.next = null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+    }
+
+    public List<Task> getTasks() {
+        List<Task> history = new ArrayList<>();
+        Node curNode = first;
+        while (curNode != null) {
+            history.add(curNode.task);
+            curNode = curNode.next;
+        }
+        return history;
+    }
 
     @Override
     public void add(Task task) {
-        historyTasks.add(task);
-        if (historyTasks.size() > historyTasksSize) {
-            historyTasks.remove(0);
-        }
+        removeNode(task.getId());
+        linkLast(task);
+        nodes.put(task.getId(), last);
     }
 
     @Override
     public List<Task> getHistory() {
-        return historyTasks;
+        return getTasks();
+    }
+
+    @Override
+    public void remove(int id) {
+        removeNode(id);
     }
 }
