e017c84dee69f5f3fb9a83a7a617dd700bfbed96
EvilG <khaninja29@yandex.ru>
2024-12-02 17:56:06 +0300

    исправлен чекстайл

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,19 @@ public class Main {
         System.out.println("Поехали!");
         TaskManager taskManager = Managers.getDefault();
 
-        Task task1 = new Task(0, "t1", StatusOfTask.NEW , "first");
-        Task task2 = new Task(0, "t2", StatusOfTask.NEW , "second");
+        Task task1 = new Task(0,"t1",StatusOfTask.NEW,"first");
+        Task task2 = new Task(0,"t2",StatusOfTask.NEW,"second");
         int task1Id = taskManager.addTask(task1);
         int task2Id = taskManager.addTask(task2);
 
-        Epic epic1 = new Epic(0, "E1",StatusOfTask.NEW, "", new ArrayList<Integer>());
-        Epic epic2 = new Epic(0, "E2",StatusOfTask.NEW, "", new ArrayList<Integer>());
+        Epic epic1 = new Epic(0,"E1",StatusOfTask.NEW,"",new ArrayList<Integer>());
+        Epic epic2 = new Epic(0,"E2",StatusOfTask.NEW,"",new ArrayList<Integer>());
         int epic1Id = taskManager.addEpic(epic1);
         int epic2Id = taskManager.addEpic(epic2);
 
-        Subtask epic1subtask1 = new Subtask(0, "Ep1S1", StatusOfTask.NEW , "", epic1Id);
-        Subtask epic1subtask2 = new Subtask(0, "Ep1S2", StatusOfTask.IN_PROGRESS , "", epic1Id);
-        Subtask epic2subtask1 = new Subtask(0, "Ep2S1", StatusOfTask.NEW , "", epic2Id);
+        Subtask epic1subtask1 = new Subtask(0,"Ep1S1",StatusOfTask.NEW,"",epic1Id);
+        Subtask epic1subtask2 = new Subtask(0,"Ep1S2",StatusOfTask.IN_PROGRESS,"",epic1Id);
+        Subtask epic2subtask1 = new Subtask(0,"Ep2S1",StatusOfTask.NEW,"",epic2Id);
         int epic1subtask1Id = taskManager.addSubtask(epic1subtask1);
         int epic1subtask2Id = taskManager.addSubtask(epic1subtask2);
         int epic2subtask1Id = taskManager.addSubtask(epic2subtask1);
